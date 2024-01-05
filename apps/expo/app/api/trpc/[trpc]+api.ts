f9d385f0e0f3b2765913084f96e7dc068a6c6022
Nishan <nishanbende@gmail.com>
2024-01-05 09:41:08 +0530

    bundle analyzer and trpc mutate

--- apps/expo/app/api/trpc/[trpc]+api.ts
+++ apps/expo/app/api/trpc/[trpc]+api.ts
@@ -3,13 +3,19 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@repo/trpc-server/routers/_app";
 
 export async function GET(req: ExpoRequest) {
-  const res = await fetchRequestHandler({
+  return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: req as unknown as Request,
     router: appRouter,
     createContext: () => ({}),
   });
+}
 
-  // TODO: convert this to ExpoResponse.
-  return res;
+export async function POST(req: ExpoRequest) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req: req as unknown as Request,
+    router: appRouter,
+    createContext: () => ({}),
+  });
 }
