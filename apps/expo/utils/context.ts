5736872c4adc0915557515579e365f7d00658e97
Nishan <nishanbende@gmail.com>
2024-06-10 12:29:32 +0530

    expo router export fix

--- apps/expo/utils/context.ts
+++
@@ -1,9 +0,0 @@
-import { CreateFastifyContextOptions } from "@trpc/server/adapters/fastify";
-
-export function createContext({ req, res }: CreateFastifyContextOptions) {
-  const user = { name: req.headers.username ?? "anonymous" };
-
-  return { req, res, user };
-}
-
-export type Context = Awaited<ReturnType<typeof createContext>>;
